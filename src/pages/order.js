675756a91b698e4c70c93fece844ac2291c2a65b
Javier Gutierrez <javier.guse@icloud.com>
2020-09-27 13:47:01 -0300

    add routing and Nav component

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function OrdersPage() {
   return (
     <div>
+      <Nav />
       <p>Hey! I'm at the home page orders page</p>
     </div>
   );
