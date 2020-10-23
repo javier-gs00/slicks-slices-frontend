8ff6546b39cc1de36bb65d01a185e8581c7599fd
Javier Gutierrez <javier.guse@icloud.com>
2020-10-22 23:09:47 -0300

    Add styled component and sanity plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in Hamilton!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'k1nzml3e',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
