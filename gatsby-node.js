436f4235a50a8d1870ead885efe0d1e9fe810719
Javier Gutierrez <javier.guse@icloud.com>
2020-12-01 22:05:19 -0300

    Generate slicemasters pages in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -54,6 +54,43 @@ async function turnToppingsIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  // query all slicemasters
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // TODO: turn each slicemaster into their own page
+  // figure out how many pages there are based on how many slicemasters there are, and how many per page
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE, 10);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+  console.log(
+    `There are ${data.slicemasters.totalCount} total people. And we have ${pageCount} pages with ${pageSize} per page`
+  );
+  // loop over from 1 to N and create the pages for them
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
+}
+
 async function fetchBeersAndTurnIntoNodes({
   actions,
   createNodeId,
@@ -90,6 +127,7 @@ export async function createPages(params) {
   await Promise.all([
     await turnPizzasIntoPages(params),
     await turnToppingsIntoPages(params),
+    await turnSlicemastersIntoPages(params),
   ]);
   // 1. pizzas
   // 2. toppings
